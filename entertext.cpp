af542c431307f30169d780cfb29bb4cc90bc5d38
Volodya-Hoi <i4masyrov@gmail.com>
2022-10-23 22:43:10 +0300

    Добавлены примеры ввода-вывода текста, циклов, условий, алгебраических операций

---
+++ entertext.cpp
@@ -0,0 +1,15 @@
+// Ввод текста и его последующий вывод
+
+#include <iostream>
+
+using namespace std;
+
+string myStr; // Обьявление переменной строки
+
+int main() {
+
+    cin >> myStr; // Ввод текста
+    cout << myStr << endl; // Вывод введеного текста
+
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+// Вывод Hello world! на экран
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello world!" << endl; // Вывод
+
+    return 0;
+}
